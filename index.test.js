55b1141607a82a79e251bbd7f82b0509411c04f5
bguilas <bryanjguilas@gmail.com>
2020-12-19 19:02:12 -0800

    mocha installed

--- index.test.js
+++ index.test.js
@@ -1,27 +1,38 @@
+const assert = require('assert');
 const {forEach, map} = require('./index');
 
-let sum = 0;
-forEach([1, 2, 3], (value) => {
-    sum += value;
-});
+// const test = (desc, fn) => {
+//     console.log('-----', desc);
+//     try {
+//         fn();
+//     } catch (e) {
+//         console.log(e.message);
+//     }
+// };
 
-if (sum !== 6) {
-    throw new Error('Expected sum of test array to equal to 6');
-}
+ it('The forEach function', () => {
+    let sum = 0;
+    forEach([1, 2, 3], (value) => {
+        sum += value;
+    });
+
+    assert.strictEqual(sum, 6);
+
+    if (sum !== 6) {
+        throw new Error('Error: Expected sum of test array to equal to 6');
+    }
+
+});
 
 //===========================================================================
-const result = map([1, 2, 3], value => {
-    return value * 2;
+
+it('The map function', () => {
+    const result = map([1, 2, 3], value => {
+        return value * 2;
+    });
+    assert.strictEqual(result[0], 2);
+    assert.strictEqual(result[1], 4);
+    assert.strictEqual(result[2], 6);
+//result === [2, 4, 6];
 });
 
-//result === [2, 4, 6];
-
-if (result[0] !== 2) {
-    throw new Error(`Expected to find 2, but found ${result[0]}`);
-}
-if (result[1] !== 4) {
-    throw new Error(`Expected to find 4, but found ${result[0]}`);
-}
-if (result[2] !== 6) {
-    throw new Error(`Expected to find 6, but found ${result[0]}`);
-}
