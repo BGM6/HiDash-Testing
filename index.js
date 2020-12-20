b31833f5fa58e9a73d505d55c3b9b23f6586cca2
bguilas <bryanjguilas@gmail.com>
2020-12-19 18:15:08 -0800

    recreated map function

--- index.js
+++ index.js
@@ -4,7 +4,17 @@ module.exports = {
             const value = arr[i];
             fn(value, i);
         }
+    },
+
+    map(arr, fn) {
+        const result = [];
+
+        for (let i = 0; i < arr.length; i++) {
+            result.push(fn(arr[i], i))
+        }
+        return result;
     }
+
 };
 
 
